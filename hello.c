baf2cde2b5f1a23bb1ed08ae67d5dd64bf9d84a8
mcai121499 <47333182+mcai121499@users.noreply.github.com>
2019-02-17 13:28:15 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,12 +2,14 @@
 int main(){
   printf("%s\n", "Hello, World.");
   int dollas = 3;
-  printf("I have $ %d", dollas);
+  printf("I have $%d\n", dollas);
   int ms = 12345678;
-  printf("I have %8d Ms in my bank account", ms);
+  printf("I have %8d Ms in my bank account\n", ms);
   float GPA = 0.0;
-  printf("Mr blutowski...%f", GPA);
+  printf("Mr blutowski...%f\n", GPA);
   float Pi = 3.1416;
-  printf("Pi is %1.4f", Pi);
+  printf("Pi is %1.4f\n", Pi);
+  float mm = .11;
+  printf("%.2f\n", mm);
   return 0;
 }
