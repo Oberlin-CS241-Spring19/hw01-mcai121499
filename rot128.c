3e13f4c2d3b7cc7b6a160bd3fc4db146ed9fd69b
mcai121499 <47333182+mcai121499@users.noreply.github.com>
2019-02-17 23:02:36 -0500

    Updated rot128.c with extra credit

--- rot128.c
+++ rot128.c
@@ -2,13 +2,23 @@
 #include <stdlib.h>
 #include <limits.h>
 
-int main(){
+int main(int argc, char**argv){
   int input = getchar();
-  
+  if(argc == 1){
   while(input != EOF ){
     int i=(input + ((UCHAR_MAX + 1)/2));
     putchar(i);
     input = getchar();
   } 
+  }
+  else{
+    while(input != EOF){
+      int shift = atoi(argv[1]);
+      int i = input + shift;
+      putchar(i);
+      input = getchar()
+    }
+  }
+      
    return 0;
 }
